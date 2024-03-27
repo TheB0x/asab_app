51ddd104c81b24253a8726e9dde80cd0101c4c79
Francisco <pachosgalvis@gmail.com>
2024-03-27 09:31:45 -0500

    Added function getSong and convert it to list

--- app/src/main/java/com/componentes/asab_app/components/AppComponents.kt
+++ app/src/main/java/com/componentes/asab_app/components/AppComponents.kt
@@ -19,9 +19,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.componentes.asab_app.data.config.FirebaseSingleton
+import com.componentes.asab_app.data.cto.SongCTO
 import com.componentes.asab_app.navigation.Screen
 import com.componentes.asab_app.ui.theme.Primary
 import com.componentes.asab_app.ui.theme.Secondary
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+
 
 @Composable
 fun ButtonNavComponent(value: String, navController: NavController, route: String){
@@ -89,14 +100,21 @@ fun ButtonSaveComponent(value: String, navController: NavController, onClick: ()
 
 
 //Search Field without conection to database//
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchComponent(){
 
+    val data = remember { mutableStateListOf<String>() }
+    LaunchedEffect(Unit) {
+        SongCTO().getDataFromFirestore(data)
+    }
+
     var text by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
     var items = remember {
-        mutableStateListOf("")
+        //mutableStateListOf("Where is my mind", "Funky Town")
+        data
     }
 
     SearchBar(
@@ -141,7 +159,6 @@ fun SearchComponent(){
                     modifier = Modifier.padding(end = 10.dp),
                     imageVector = Icons.Default.History,
                     contentDescription = "History Icon")
-
                 Text(text = it)
             }
         }
